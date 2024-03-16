0b0e50d780cdadd3530da965f27cf7aff50db47d
Gabriel720-tech <angelorioscano64@gmail.com>
2024-03-16 16:00:46 -0500

    segundo commit

--- app/src/main/java/pe/edu/idat/appgrupo2/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/pe/edu/idat/appgrupo2/ui/gallery/GalleryFragment.kt
@@ -1,20 +1,21 @@
 package pe.edu.idat.appgrupo2.ui.gallery
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import pe.edu.idat.appgrupo2.R
 import pe.edu.idat.appgrupo2.databinding.FragmentGalleryBinding
+import java.util.Calendar
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,15 +29,42 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Obtener el contexto de requireContext() o requireActivity()
+        ArrayAdapter.createFromResource(requireContext(), R.array.dias, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spdia.adapter = adapter
         }
+
+        ArrayAdapter.createFromResource(requireContext(), R.array.cantiPerros, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spcantiperros.adapter = adapter
+        }
+
+        binding.btnfecha.setOnClickListener {
+            mostrarDatePicker()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun mostrarDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Crear y mostrar el DatePickerDialog
+        val datePickerDialog = DatePickerDialog(requireContext(), { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            // Actualizar el texto del EditText con la fecha seleccionada
+            val fechaSeleccionada = "$dayOfMonth/$month/$year"
+            binding.tvFecha.setText(fechaSeleccionada)
+        }, year, month, dayOfMonth)
+
+        datePickerDialog.show()
     }
 }
