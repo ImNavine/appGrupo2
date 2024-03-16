b0aa45ed9ac54087771b3c3c3fd1cb966521bf8f
ImNavine <kevinadine14@gmail.com>
2024-03-16 12:05:11 -0500

    Listado Fragment Terminado

--- app/src/main/java/pe/edu/idat/appgrupo2/ListadoFragment.kt
+++ app/src/main/java/pe/edu/idat/appgrupo2/ListadoFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.appgrupo2.databinding.FragmentListadoBinding
 
 class ListadoFragment : Fragment() {
-
+    private var _binding: FragmentListadoBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listado, container, false)
+        _binding=FragmentListadoBinding.inflate(inflater, container, false)
+        binding.rvlistado.layoutManager= LinearLayoutManager(requireContext())
+        binding.rvlistado.adapter=AdapterResena(listaResena(), requireContext())
+        return binding.root
+    }
+
+    fun listaResena(): List<Resena> {
+    var lista=ArrayList<Resena>()
+        lista.add(Resena("Kevin", "Prueba de descripcion",
+            "16/03/2024", R.drawable.user1))
+        lista.add(Resena("Kevin", "Prueba de descripcion",
+            "16/03/2024", R.drawable.user2))
+        lista.add(Resena("Kevin", "Prueba de descripcion",
+            "16/03/2024", R.drawable.user3))
+        lista.add(Resena("Kevin", "Prueba de descripcion",
+            "16/03/2024", R.drawable.user4))
+        return lista
     }
 
 }
